3ddd636d73285a30e33bbe91ac224d9af3bd0d59
me2sumanislam <me2sumanislam.247@gmail.com>
2026-02-16 12:53:48 +0600

    work start on js

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
 //  console.log("js add hoiche ")
+
+fetch("https://fakestoreapi.com/products")
+.then((res) => res.json())
+.then((products) => {
+
+products.forEach(products) => {
+    console.log(products.rating.rate)
+}
+
+
+
+})
