de2a23c2fea2731a9c905a285ae388bfe18c66c2
me2sumanislam <me2sumanislam.247@gmail.com>
2026-02-17 07:58:15 +0600

    add 4 button dynamicaly done

--- script/product.js
+++ script/product.js
@@ -1,1 +1,33 @@
  
+ const loadCategories = () =>{
+    fetch ("https://fakestoreapi.com/products/categories")
+    .then((res) => res.json())
+    .then((data) => showButton(data))
+ }
+  
+ const showButton = (btnCatagories) =>{
+    // console.log(btnCatagories) //ok data asche
+    const container = document.getElementById("4btn-Container");
+    container.innerHTML = "";
+
+ const fourButton = btnCatagories.slice(0, 4)
+
+    for( let btncatagorie of fourButton ){
+        // console.log(btncatagorie); //ok catagories 
+
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+
+        <button class =" btn rounded-xl px-5 py-2 border" >
+        ${btncatagorie}
+        </button>
+        
+        `;
+        container.appendChild(div);
+    }
+ }
+
+
+
+ loadCategories();
